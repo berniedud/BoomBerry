b7dba124691fc05637e098ab618a8b4a0688608e
Bernard Dudley <bernard.dudley@tvsquared.com>
2018-03-20 00:19:44 +0100

    make waves

--- octolib.py
+++ octolib.py
@@ -106,3 +106,58 @@ def flash_element(mc, element, value, duration=0.4):
     set_element(mc, flash_condition, element, min_map, max_map)
     sleep(duration)
     set_element(mc, prior_condition, element, min_map, max_map)
+
+
+# from pprint import pprint
+def make_wave(mc, element, centre=(0, 0), steps=20, magnitude=20, duration=5.0):
+    wave_shape = sin_wave(steps, magnitude)
+    def get_wave_value(i):
+        try:
+            assert i >= 0
+            return wave_shape[i]
+        except:
+            return 0
+    get_wave_map = np.vectorize(get_wave_value)
+    prior_condition = mc.get(element)
+    distance_map = make_distance_map(prior_condition, centre)
+    # pprint('distance_map')
+    # pprint(distance_map)
+    # exit()
+    min_distance = distance_map.min()
+    max_distance = distance_map.max()
+    total_steps = steps + max_distance - min_distance
+    sleep_time = float(duration) / total_steps
+    for step in range(total_steps) + min_distance:
+        index_map = (step - distance_map + 1) * np.less_equal(distance_map, step) - 1
+        # index_map = index_map * np.less_equal(index_map, total_steps)
+        increment_map = get_wave_map(index_map)
+        # pprint('step: {}'.format(step))
+        # pprint('prior:')
+        # pprint(prior_condition)
+        # pprint('index_map:')
+        # pprint(index_map)
+        # pprint('increment:')
+        # pprint(increment_map)
+        # exit()
+        new_condition = prior_condition + increment_map
+        mc.set(element, new_condition)
+        sleep(sleep_time)
+        prior_condition = mc.get(element)
+
+
+def sin_wave(steps=20, magnitude=20):
+    steps = (int(steps) / 4 * 4) or 4
+    ix = np.arange(steps + 1)
+    signal = np.sin(2 * np.pi * ix / steps) * magnitude
+    diffs = np.diff(signal)
+    return diffs.astype(int)
+
+
+def make_distance_map(target_array, centre):
+    centre = np.array(centre)
+    distance_map = np.zeros_like(target_array)
+    for i in range(target_array.shape[0]):
+        for j in range(target_array.shape[1]):
+            distance_map[i, j] = np.linalg.norm(centre - (i, j))
+
+    return distance_map
