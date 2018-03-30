500f7e397d9caa43262c776e42f9d517a6b4cefb
Bernard Dudley <bernard.dudley@tvsquared.com>
2018-03-30 18:51:04 +0100

    send to hat

--- tools/display_rgb_grids.py
+++ tools/display_rgb_grids.py
@@ -5,7 +5,7 @@ from octolib import mc
 
 SLEEPTIME = 0.2
 
-def display_grids():
+def display_grids_print():
     while True:
         for colour in ['red','green','blue']:
             pprint(colour)
@@ -13,5 +13,24 @@ def display_grids():
 
         sleep(SLEEPTIME)
 
+def display_on_hat():
+    red = mc.get('red')
+    green = mc.get('green')
+    blue = mc.get('blue')
+    pixels = []
+    for y in range(red.shape[1]):
+        row = []
+        for x in range(red.shape[0]):
+            pixel = (red[x][y], green[x][y], blue[x][y])
+            row.append(pixel)
+        pixels.append(row)
+    print(pixels)
+    un.set_pixels(pixels)
+    
+
 if __name__ == '__main__':
-    display_grids()
+    try:
+        import unicornhat as un
+        display_on_hat()
+    except:
+        display_grids_print()
