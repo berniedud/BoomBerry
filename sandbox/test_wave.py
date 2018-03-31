58a71a595afba68c2780b37a85360491ff2e776e
Bernard Dudley <bernie@tech-turtle.co.uk>
2018-03-31 10:06:37 +0000

    more messing

--- sandbox/test_wave.py
+++ sandbox/test_wave.py
@@ -1,6 +1,9 @@
 from octolib import mc, reset_zeroes, make_wave
 
-reset_zeroes(mc)
+#reset_zeroes(mc)
 
-make_wave(mc, 'blue', centre=(0,0), duration=4, steps=40, magnitude=100, iterations=2)
+while True:
+    make_wave(mc, 'blue', centre=(0,0), duration=4, steps=60, magnitude=100, iterations=3)
+    make_wave(mc, 'red', centre=(0,7), duration=4, steps=60, magnitude=100, iterations=3)
+    make_wave(mc, 'green', centre=(7,7), duration=4, steps=60, magnitude=100, iterations=3)
 
