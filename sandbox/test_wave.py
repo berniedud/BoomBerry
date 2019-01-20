bb5ff856316e60ef0103d31b56140a71f2a0dc26
Bernard Dudley <bernard.dudley@tvsquared.com>
2019-01-20 21:52:36 +0000

    testing

--- sandbox/test_wave.py
+++ sandbox/test_wave.py
@@ -2,5 +2,5 @@ from octolib import mc, reset_zeroes, make_wave
 
 reset_zeroes(mc)
 
-make_wave(mc, 'blue', centre=(0,0), duration=4, steps=40, magnitude=100, iterations=2)
+make_wave(mc, 'blue', centre=(0,0), duration=10, steps=20, magnitude=100, iterations=2)
 
